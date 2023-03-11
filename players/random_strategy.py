3e812ba5840b1f295e65ea778980570f5fa966fd
GrayJack <gr41.j4ck@gmail.com>
2023-03-11 00:26:52 -0300

    feat: Add Python implementation of the random strategy

---
+++ players/random_strategy.py
@@ -0,0 +1,22 @@
+#!env python3
+
+# Copyright (C) 2023  Diogo F. S. Ramos and UFABC Dojo Members
+
+import random
+
+EMPTY = "."
+ALL_MOVES = range(1, 8)
+
+
+def is_legal(move: int, board: str) -> bool:
+    """Check if `move` is legal."""
+    return board[move - 1] == EMPTY
+
+
+def random_strategy(board: str) -> int:
+    """Make a random move."""
+    return random.choice([move for move in ALL_MOVES if is_legal(move, board)])
+
+
+while True:
+    print(random_strategy(input()[2:]))
