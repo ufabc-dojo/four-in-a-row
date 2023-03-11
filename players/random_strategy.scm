6054ee7aaf80d4511c7b912d76860ba8768ba844
GrayJack <gr41.j4ck@gmail.com>
2023-03-11 00:27:17 -0300

    feat: Add Guile Scheme implementation of the random strategy

---
+++ players/random_strategy.scm
@@ -0,0 +1,25 @@
+#!env guile -s
+!#
+
+; Copyright (C) 2023  Diogo F. S. Ramos and UFABC Dojo Members
+
+(use-modules (ice-9 rdelim)
+             (srfi srfi-1))
+
+(define empty #\.)
+(define all-moves (iota 7 1))
+
+(define (random-ref list)
+  (list-ref list (random (length list))))
+
+(define (valid? move board)
+  (char=? empty (string-ref board (1- move))))
+
+(define (random-strategy board)
+  (random-ref (filter (lambda (move) (valid? move board))
+                      all-moves)))
+
+(set! *random-state* (random-state-from-platform))
+(while #t
+  (write-line (random-strategy (substring (read-line) 2)))
+  (force-output))
